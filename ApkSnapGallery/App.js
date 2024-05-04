30db84bbe0fcfdfd1d1854d6983cdfcbd6c44513
Cabrera437 <124718225+Cabrera437@users.noreply.github.com>
2024-05-04 17:12:31 -0600

    Add files via upload

---
+++ ApkSnapGallery/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bienvenido a nuestra aplicación SnapGallery!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FF9999',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
